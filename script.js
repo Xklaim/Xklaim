43995505d04ee1a3dbf71aaa9ea591d6cd908a88
Xklaim <162603644+Xklaim@users.noreply.github.com>
2024-03-16 15:55:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,88 @@
+// let a = 50;
+// let b = 5;
+// let mode = "any";
+// let num = 15;
+
+// console.log("a + b=", a + b)
+// console.log("a - b=", a - b)
+// console.log("a * b=", a * b)
+// console.log("a / b=", a / b)
+
+// console.log("a modulus % b=", a % b)
+// console.log("a exponential b=", a ** b)
+// console.log("a ++ b=", ++a)
+// console.log("a -- b=", --b)
+
+// console.log(a>b)
+
+// console.log(!(a>b))
+
+// if (mode === "dark") {
+//     console.log("grey");
+// } else if (mode === "pink") {
+//     console.log("pink");
+// } else if (mode === "orange") { console.log("orange");}
+// else{
+//     console.log("white")
+// }
+
+// if (num > 10) {
+//     console.log("Number is greater than 10");
+// } else if (num < 10) {
+//     console.log("Number is less than 10");
+// } else {
+//     console.log("Number is equal to 10");
+// }
+
+// console.log(num >= 10 ? "number greater than or equal to 10" : "number not greater than 10")
+
+// console.log(num <= 10 ? "number lesser than or equal to 10" : "number not lesser than 10")
+// console.log(num >= 200 ? "number greater than or equal to 20" : "number not greater than 20")
+
+
+
+
+
+
+// let age = 17;
+
+// if (age < 18) {
+//   console.log("you cant vote kiddo");
+// } else if (age >= 18 && age <= 60) {
+//   console.log(" you can vote");
+// } else {
+//   console.log(" get outta here oldie!");
+// }
+
+
+// let num =1;
+
+// for (num=1; num<21; num++){
+//     if (num%2==1) {
+//     console.log(num)
+//     }
+// }
+
+// while (num<21){
+//     if (num%2==0){
+//     console.log(num)
+//      }
+//     num++
+// }
+
+
+// const position = 4;
+
+// switch (position){
+//     case (1):
+//         console.log("it is one");
+//         break;
+//     case (2):
+//         console.log("it is 2");
+//         break;
+//     case (3):
+//         console.log("it is three");
+//         break;
+//     default:
+//         console.log("you didnt score any position you were", (position-3),"spot behind number three");
+// }
